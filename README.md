56da1e62c948050e597da6d98881ef5d4c1d1365
jD91mZM2 <me@krake.one>
2018-01-12 17:05:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# [rot26](http://rot26.org/)
+
+> ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet.
+> Instead of only rotating 13 places, ROT26 rotates twice as many characters in the alphabet and is therefore twice as secure.
+
+Pure rust rewrite of the rot26 algorithm.  
+Even maintains support for rot13 and *any* rot, with friendly helpful comments advising you to just stick to rot26.
+
+> ROT26 encryption & decryption is very complex and requires a powerful, purpose-built super-computer to perform all the calculations... which we have created.
+> So, to encourage more developers to use ROT26 in their mobile, web and PC software applications we are offering a very easy to use and totally free ROT26 encryption and decryption REST web service.
+
+This is no longer true.  
+That and all the following are features now possible thanks to Rust:
+
+ - Complete unicode support. Disregards any non-alphabetical symbols! (was probably possible before actually)
+ - Unit tests.
---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+const ROTATE: u32 = 'z' as u32 - 'a' as u32 + 1;
+
+/// Encrypts the input using rot26.
+#[inline(always)]
+pub fn encrypt(input: &str) -> String {
+    encrypt_any(input, 26)
+}
+
+/// Decrypts the input using rot26.
+#[inline(always)]
+pub fn decrypt(input: &str) -> String {
+    decrypt_any(input, 26)
+}
+
+/// Encrypts the input using rot13.
+/// Warning: Security researchers have managed to crack rot13.
+/// New users are recommended to use rot26 for the best security.
+#[inline(always)]
+pub fn encrypt_rot13(input: &str) -> String {
+    encrypt_any(input, 13)
+}
+
+/// Decrypts the input using rot13.
+/// Warning: Security researchers have managed to crack rot13.
+/// New users are recommended to use rot26 for the best security.
+#[inline(always)]
+pub fn decrypt_rot13(input: &str) -> String {
+    decrypt_any(input, 13)
+}
+
+/// Encrypt using any amount.
+/// Warning: Please carefully choose the right amount.
+/// New users are recommended to use rot26 for the best security.
+pub fn encrypt_any(input: &str, amount: u32) -> String {
+    input.chars().map(|c| {
+        let base = if c > 'a' && c < 'z' {
+            'a' as u32
+        } else if c > 'A' && c < 'Z' {
+            'A' as u32
+        } else {
+            return c;
+        };
+
+        std::char::from_u32(((c as u32 - base + amount) % ROTATE) + base).unwrap()
+    }).collect()
+}
+
+/// Decrypt using any amount.
+/// Warning: Please carefully choose the right amount.
+/// New users are recommended to use rot26 for the best security.
+pub fn decrypt_any(input: &str, amount: u32) -> String {
+    input.chars().map(|c| {
+        let base = if c > 'a' && c < 'z' {
+            'a' as u32
+        } else if c > 'A' && c < 'Z' {
+            'A' as u32
+        } else {
+            return c;
+        };
+
+        std::char::from_u32(((c as u32 - base + ROTATE - amount) % ROTATE) + base).unwrap()
+    }).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use ::*;
+
+    #[test]
+    fn test_rot26() {
+        let plain = "hello";
+        let encrypted = encrypt(plain);
+
+        assert_eq!(encrypted, "hello");
+
+        let decrypted = decrypt(&encrypted);
+
+        assert_eq!(plain, decrypted);
+    }
+    #[test]
+    fn test_rot13() {
+        let plain = "hello";
+        let encrypted = encrypt_rot13(plain);
+
+        assert_eq!(encrypted, "uryyb");
+
+        let decrypted = decrypt_rot13(&encrypted);
+
+        assert_eq!(plain, decrypted);
+    }
+    #[test]
+    fn test_rot_any() {
+        let amount = 1;
+
+        let plain = "hello";
+        let encrypted = encrypt_any(plain, amount);
+
+        assert_eq!(encrypted, "ifmmp");
+
+        let decrypted = decrypt_any(&encrypted, amount);
+
+        assert_eq!(plain, decrypted);
+    }
+}
